df88a3c964b8f67c9d46ee54dea9693ae273fe56
Berkan Öztürk <101871080+berkanozturkk@users.noreply.github.com>
2024-03-20 17:38:02 +0300

    Delete securedlogin directory

--- securedlogin/src/main/java/securedlogin/repo/UserRepo.java
+++
@@ -1,9 +0,0 @@
-package securedlogin.repo;
-
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepo {
-
-    public int logincheck(String username, String password);
-}
